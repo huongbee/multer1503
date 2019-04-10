afcd4e5e5d21d5549eda0dd50bcab5be5242a1dd
Huong <huongnguyenak96@gmail.com>
2019-04-10 19:33:14 +0700

    fileFilter limits

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ const storageImage = multer.diskStorage({
         cb(null, Date.now()+'-'+file.originalname)
     }
 })
-const upload = multer({storage:storageImage})
+/**
+ * filter: mime type, check file size
+ * rename file
+ */
+const fileFilter = (req,file,cb)=>{
+    if(file.mimetype == 'image/png' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/gif' || file.mimetype == 'image/jpg'){
+        return cb(null,true)
+    }
+    return cb(new Error('File not allow!'))
+}
+const upload = multer({storage:storageImage, fileFilter, limits:{fileSize:100*1024} }) //100kb
 
 const app = express();
 app.set('view engine','ejs');
@@ -21,15 +31,22 @@ app.get('/upload-file',(req,res)=>{
     res.render('upload');
 })
 
+const single = upload.single('image') // function(res,res,()=>{})
+app.post('/upload-file',(req,res)=>{
+    single(req,res,function(err){
+        if(err) return res.send({error: err.message})
+
+        const image = req.file 
+        res.send({image})
+    })
+})
+
 // app.post('/upload-file',upload.single('image'),(req,res)=>{
 //     const image = req.file 
 //     const name = req.body.txtname 
 //     res.send({image,name})
 // });
-/**
- * filter: mime type, check file size
- * rename file
- */
+
 // app.post('/upload-file',upload.array('image',4),(req,res)=>{
 //     const arrImage = req.files 
 //     res.send({arrImage})
@@ -42,9 +59,9 @@ const config = upload.fields([{
     maxCount: 2
 }]);
 
-app.post('/upload-file',config,(req,res)=>{
-    const arrImage = req.files 
-    res.send({arrImage})
-});
+// app.post('/upload-file',config,(req,res)=>{
+//     const arrImage = req.files 
+//     res.send({arrImage})
+// });
 
 app.listen(3000,()=>console.log('Server started!'))
